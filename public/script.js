59a94a58bab8f8f64b4ac5f9fc99e7bd858e2e20
System Administrator <root@Joses-MacBook-Pro.local>
2018-04-07 17:21:11 -0700

    added script

--- public/script.js
+++ public/script.js
@@ -1,59 +1,47 @@
-alert("hello");
-// document.addEventListener('DOMContentLoaded', getCurrentLocation);
+document.addEventListener('DOMContentLoaded', getCurrentLocation);
 
-// function getCurrentLocation() {
+       function getCurrentLocation(){
+        alert("hello");
 
-// 	//Source: https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
-// 	// In the following line, you should include the prefixes of implementations you want to test.
-// 	window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// 	// DON'T use "var indexedDB = ..." if you're not in a function.
-// 	// Moreover, you may need references to some window.IDB* objects:
-// 	window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
-// 	window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-// 	// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
+        //Source: https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+        // In the following line, you should include the prefixes of implementations you want to test.
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+        // DON'T use "var indexedDB = ..." if you're not in a function.
+        // Moreover, you may need references to some window.IDB* objects:
+        window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+        window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+        // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
 
-// 	if (!window.indexedDB) {
-// 	    window.alert("Your browser doesn't support a stable version of IndexedDB");
-// 	}
-//     navigator.geolocation.getCurrentPosition(getNearbyConcerts);
-// }
+        if (!window.indexedDB) {
+            window.alert("Your browser doesn't support a stable version of IndexedDB");
+        }
+            navigator.geolocation.getCurrentPosition(getNearbyConcerts);
+        }
+        function getNearbyConcerts(currentLocation){
+            console.log(currentLocation);
+            let long = currentLocation.coords.longitude;
+            let lat = currentLocation.coords.latitude;
+            let tours = {};
 
-// function getNearbyConcerts(currentLocation){
-// 	console.log(currentLocation);
-	 // var req = new XMLHttpRequest(); 
-	 // var url = "/concerts/" + currentLocation;
-	 // req.open("GET", url, true);
-	 // req.addEventListener('load', function() {
-	 //   if(req.status >= 200 && req.status < 400){
-	 //     var response = JSON.parse(req.responseText);
-	 //     console.log(response.results);
-	 //   } else {
-	 //       console.log("Error in network request: " + req.statusText);
-	 //   }
-	 // }); 
-	 // req.send(null);
+            console.log(long, lat);
+            $.getJSON(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=8GeWhE6iAVe1C4OaVvktBf
+            auENCKgwxw&latlong=${lat},${long}&classificationName=music`)
+            .done(data => {
+                data._embedded.events.forEach(event => {
+                    // console.log(event.name);
+                    if (!tours[event.name]){
+                        tours[event.name] = [];
+                        tours[event.name].push(event.dates.start);
+                    }
+                    else {
+                        tours[event.name].push(event.dates.start);
+                    }
+                });           
+                console.log(tours);
+     
+            })
 
-	 //  $.ajax({
-  //       url: "/concerts/",
-  //       type: "POST",
-  //       data: currentLocation,
-  //       dataType: "json",
-  //       success: function (result) {
-  //           switch (result) {
-  //               case true:
-  //                   processResponse(result);
-  //                   break;
-  //               default:
-  //                   resultDiv.html(result);
-  //           }
-  //       },
-  //       error: function (xhr, ajaxOptions, thrownError) {
-  //       alert(xhr.status);
-  //       alert(thrownError);
-  //       }
-  //   });
-
-// }
+        }
 
 
 // function getTopTracks(event) {
